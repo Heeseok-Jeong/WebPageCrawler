0ffd127fa0c0928b51eec41c53aeac03067b1e8a
heeseok <heeseok@218.38.137.27>
2018-06-18 23:15:34 +0900

    set Apache commons CLI

---
+++ src/main/java/edu/handong/csee/java/webpagecrawler/RunnerOfWebPageCrawler.java
@@ -0,0 +1,91 @@
+package edu.handong.csee.java.webpagecrawler;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+public class RunnerOfWebPageCrawler {
+	String url;
+	String savePath;
+	boolean help;
+	
+	public static void main(String[] args) {
+		RunnerOfWebPageCrawler runner = new RunnerOfWebPageCrawler();
+		runner.doJob(args);
+	}
+	
+	private void doJob(String[] args) {
+		//APACHE COMMONS CLI
+		Options options = createOptions();
+		if(parseOptions(options, args)){
+			if (help){
+				printHelp(options);
+				return;
+			}
+			// path is required (necessary) data so no need to have a branch.
+			System.out.println("You provided \"" + url + "\" as the value of the option u");
+
+			// path is required (necessary) data so no need to have a branch.
+			System.out.println("You provided \"" + savePath + "\" as the value of the option d");
+		}
+	}
+	
+	private boolean parseOptions(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+
+		try {
+
+			CommandLine cmd = parser.parse(options, args);
+
+			url = cmd.getOptionValue("u");
+			savePath = cmd.getOptionValue("d");
+			help = cmd.hasOption("h");
+
+		} catch (Exception e) {
+			printHelp(options);
+			return false;
+		}
+
+		return true;
+	}
+	
+	// Definition Stage
+		private Options createOptions() {
+			Options options = new Options();
+
+			// add options by using OptionBuilder
+			options.addOption(Option.builder("i").longOpt("input")
+					.desc("Set a directory path that contains input files")
+					.hasArg()
+					.argName("Directory Path")
+					.required()
+					.build());
+
+			// add options by using OptionBuilder
+			options.addOption(Option.builder("o").longOpt("output")
+					.desc("Set a directory path that output file are saved")
+					.hasArg()
+					.argName("Directory Path")
+					.required()
+					.build());
+
+			// add options by using OptionBuilder
+			options.addOption(Option.builder("h").longOpt("help")
+					.desc("Help")
+					.build());
+
+			return options;
+		}
+
+		private void printHelp(Options options) {
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			String header = "Counting messages of CacaoTalk per person program";
+			String footer ="\nPlease report issues at dldydldy@naver.com";
+			formatter.printHelp("CountMessages", header, options, footer, true);
+		}
+
+}
